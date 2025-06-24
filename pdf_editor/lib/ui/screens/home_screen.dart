c2d058a6c3c8a897dbe2983f566594bcbf78a53e
Norbert Orosz <norbert.orosz1@tesco.com>
2025-06-24 06:49:28 +0200

    load-pdf: drag and drop pdf file

--- pdf_editor/lib/ui/screens/home_screen.dart
+++ pdf_editor/lib/ui/screens/home_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:pdf_editor/ui/widget/file_browser.dart';
+import 'package:pdf_editor/ui/widget/drag_and_drop_area.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  void _handlePdfSelected(String filePath) {
+    debugPrint("Selected PDF: $filePath");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: FileBrowser()));
+    // return Scaffold(body: Center(child: FileBrowser()));
+    return Scaffold(
+      body: Center(child: PdfDropTargetAndPicker(onPdfSelected: _handlePdfSelected)),
+    );
   }
 }
